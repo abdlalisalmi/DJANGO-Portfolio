8b5d3d8dd48edca9866b466b371811f9a9efb3da
Abdelaali Es salmi <abdlalisalmi@gmail.com>
2020-12-05 20:52:45 +0100

    finish porfie page

--- portfolio/static/dashboard.js
+++ portfolio/static/dashboard.js
@@ -1,5 +1,5 @@
 // edit profile code
-var saveProfileEdit = document.querySelector('#save-profile-edit');
+const saveProfileEdit = document.querySelector('#save-profile-edit');
 if (saveProfileEdit) {
 	saveProfileEdit.addEventListener('click', (e) => {
 		e.preventDefault();
@@ -138,19 +138,19 @@ if (messageSearchBtn) {
 
 
 
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-const csrftoken = getCookie('csrftoken');
+// function getCookie(name) {
+//     let cookieValue = null;
+//     if (document.cookie && document.cookie !== '') {
+//         const cookies = document.cookie.split(';');
+//         for (let i = 0; i < cookies.length; i++) {
+//             const cookie = cookies[i].trim();
+//             // Does this cookie string begin with the name we want?
+//             if (cookie.substring(0, name.length + 1) === (name + '=')) {
+//                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//                 break;
+//             }
+//         }
+//     }
+//     return cookieValue;
+// }
+// const csrftoken = getCookie('csrftoken');
