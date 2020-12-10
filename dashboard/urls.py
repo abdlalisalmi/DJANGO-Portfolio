e74de5b9e76f626ad9d60703766a3fe479fe37d3
Abdelaali Es salmi <abdlalisalmi@gmail.com>
2020-12-10 13:10:50 +0100

    add logout and home buttons to dashboard sidebar

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from .views import (
     dashboard,
@@ -23,4 +24,6 @@ urlpatterns = [
 
     path('projects', projects, name='projects'),
     path('projects/api/', projects_api, name='projects_api'),
+
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
