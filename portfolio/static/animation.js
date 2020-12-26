28c64f503611717b62149d812845416df5f77170
Abdelaali Es Salmi <abdlalisalmi@gmail.com>
2020-12-26 12:26:41 +0100

    add class effect to all cards

--- portfolio/static/animation.js
+++ portfolio/static/animation.js
@@ -3,7 +3,7 @@
 particlesJS("particles-js", {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 60,
         "density": {
           "enable": true,
           "value_area": 1000
